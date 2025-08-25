5a3ae803e30237f6f8e3a26845b857f5f85ffbc4
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-08-25 15:32:41 +0530

    Added toggleCommentLike functionality to handle liking and unliking comments in like.controller.js

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -25,3 +25,32 @@ const toggleVideoLike  =asyncHandler (async(req,res)=>{
     .status(201)
     .json(new ApiResponse(201, like, "Video liked successfully"));
 })
+
+const toggleCommentLike =asyncHandler(async(req,res)=>{
+
+     const { commentId } = req.params;
+  const userId = req.user._id;
+
+  
+  const existingLike = await Like.findOne({ comment: commentId, likedBy: userId }); //likeByiD ka matlab konse user ne like kiya hain aur ye user se ref ke id le rha hain
+
+  if (existingLike) {
+    // Unlike
+    await existingLike.deleteOne();
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Comment unliked successfully"));
+  }
+
+   const like = await Like.create({ comment: commentId, likedBy: userId });
+  return res
+    .status(201)
+    .json(new ApiResponse(201, like, "Comment liked successfully"));
+})
+
+
+export {
+    toggleVideoLike,
+    toggleCommentLike,
+
+}
