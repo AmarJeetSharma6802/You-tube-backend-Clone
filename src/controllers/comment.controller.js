d183288b952f83c7714460d8e03d55e315faab6f
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2025-08-25 15:13:18 +0530

    Refactored comment.controller.js for improved readability and added likes count to video comments response

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,79 +1,88 @@
-import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
 
-    const options = {
-        page:parseInt(page),
-        limit:parseInt(limit)
-    }
-    const comments = await Comment.find({video:videoId})
+  const options = {
+    page: parseInt(page),
+    limit: parseInt(limit),
+  };
+  const comments = await Comment.find({ video: videoId })
     .limit(options.limit)
     .skip((options.page - 1) * options.limit)
-    .populate('likedBy')
-    .exec()
-    res.status(200).json(new ApiResponse(comments))
-})
+    // .populate('likedBy')
+    .populate("owner", "username email")
+    .exec();
+
+    // like count 
+  let commentsWithLikes = [];
+
+  for (let comment of comments) {
+    const likesCount = await Like.countDocuments({ comment: comment._id });
+
+    commentsWithLikes.push({
+      ...comment.toObject(),
+      likesCount,
+    });
+  }
+
+  res.status(200).json(new ApiResponse(comments));
+});
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
+  // TODO: add a comment to a video
 
-    const {videoId} =req.params
-    const {content} = req.body
+  const { videoId } = req.params;
+  const { content } = req.body;
 
-    const newComment =  await Comment.create({
-        video:videoId,
-        content:content,
-        // likedBy:[req.user._id],
-        likedBy:req.user._id,
-    })
+  const newComment = await Comment.create({
+    video: videoId,
+    content: content,
+    // likedBy:[req.user._id],
+    likedBy: req.user._id,
+  });
 
-    res.status(201).json(new ApiResponse(newComment));
-
-})
+  res.status(201).json(new ApiResponse(newComment));
+});
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-    const { commentId } = req.params; // Assume the comment ID is passed in the URL
-    const { content } = req.body;
+  // TODO: update a comment
+  const { commentId } = req.params; // Assume the comment ID is passed in the URL
+  const { content } = req.body;
 
-    // Find and update the comment
-    const updatedComment = await Comment.findByIdAndUpdate(
-        commentId,
-        { content },
-        { new: true, runValidators: true } // Return the updated document
-    );
+  // Find and update the comment
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    { content },
+    { new: true, runValidators: true } // Return the updated document
+  );
 
-    if (!updatedComment) {
-        throw new ApiError(404, "Comment not found");
-    }
+  if (!updatedComment) {
+    throw new ApiError(404, "Comment not found");
+  }
 
-    res.status(200).json(new ApiResponse(updatedComment));
-})
+  res.status(200).json(new ApiResponse(updatedComment));
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
-    const { commentId } = req.params;
+  // TODO: delete a comment
+  const { commentId } = req.params;
 
-    // Find and delete the comment
-    const deletedComment = await Comment.findByIdAndDelete(commentId);
+  // Find and delete the comment
+  const deletedComment = await Comment.findByIdAndDelete(commentId);
 
-    if (!deletedComment) {
-        throw new ApiError(404, "Comment not found");
-    }
+  if (!deletedComment) {
+    throw new ApiError(404, "Comment not found");
+  }
 
-    res.status(204).send(); // No content to send back
-})
+  res.status(204).send(); // No content to send back
+});
 
-export {
-    getVideoComments, 
-    addComment, 
-    updateComment,
-     deleteComment
-    }
+export { getVideoComments, addComment, updateComment, deleteComment };
